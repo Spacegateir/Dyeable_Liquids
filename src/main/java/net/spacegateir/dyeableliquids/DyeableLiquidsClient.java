072b677cf3cf793861218fccd7ccc3e41ddfc7fd
spacegateir <1Spacegateir@gmail.com>
2025-05-25 19:16:04 +0930

    Initial commit

---
+++ src/main/java/net/spacegateir/dyeableliquids/DyeableLiquidsClient.java
@@ -0,0 +1,11 @@
+package net.spacegateir.dyeableliquids;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DyeableLiquidsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/spacegateir/dyeableliquids/DyeableLiquidsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spacegateir.dyeableliquids;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DyeableLiquidsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
